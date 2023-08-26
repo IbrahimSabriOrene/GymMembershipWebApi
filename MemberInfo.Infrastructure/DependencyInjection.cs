14267b5bb3036599ec5f96563f8c33b922da872b
IbrahimSabriOrene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-08-26 15:54:07 +0300

    Added AddPersistence for clear understanding

--- MemberInfo.Infrastructure/DependencyInjection.cs
+++ MemberInfo.Infrastructure/DependencyInjection.cs
@@ -18,9 +18,17 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
         services.AddAuth(configuration);
+        services.AddPersistence();
         services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
-        services.AddScoped<IUserRepository, UserRepository>();
+        
         return services;
+    }
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        return services;
+
     }
 
     public static IServiceCollection AddAuth(this IServiceCollection services, ConfigurationManager configuration)
