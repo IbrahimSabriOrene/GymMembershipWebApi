1d3033903df798c0dc2e4597c9d0c098e37f7227
IbrahimSabriOrene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-08-26 15:51:32 +0300

    Basic repo implementation

--- MemberInfo.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
+++ MemberInfo.Application/Products/Commands/CreateProduct/CreateProductCommandHandler.cs
@@ -1,5 +1,6 @@
 using ErrorOr;
 using MediatR;
+using MemberInfo.Application.Common.Interfaces.Persistence;
 using MemberInfo.Domain.Person.ValueObjects;
 using MemberInfo.Domain.Products;
 using MemberInfo.Domain.Products.ValueObjects;
@@ -8,14 +9,26 @@ namespace MemberInfo.Application.Products.Commands.CreateProduct;
 
 public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ErrorOr<Product>>
 {
-    public Task<ErrorOr<Product>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    private readonly IProductRepository _productRepository;
+
+    public CreateProductCommandHandler(IProductRepository productRepository)
     {
+        _productRepository = productRepository;
+    }
+
+    public async Task<ErrorOr<Product>> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
         var product = Product.Create(
             productName: request.ProductName,
-            prices: request.Price.ConvertAll(x => Price.Create(x.Amount, x.Currency)).ToList(),
+            prices: request.Price.ConvertAll(x => Price.Create(x.Amount, x.Currency)),
             months: request.Months,
-            personIds: request.PersonIdList.ConvertAll(x => PersonId.CreateUnique(x.Id)) // Problem: PersonIdList can not be null, but it is null.
+            personIds: request.PersonIds.ConvertAll(x => PersonId.CreateUnique(x.Id))// Problem: PersonIdList can not be null, but it is null.
             );
+
+            _productRepository.Add(product);
+
+          //what we could do=> PersonId.CreateUnique(request.PersonId.Id) but if we do this we are only passing one personId.
             
 
             //Possible way to do this: Change createUnique to create and pass the person's id as a parameter.
@@ -23,6 +36,6 @@ public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand,
             //Problems with the above line:
             //Lets make it work for now
             // We are gonna implement the user's guid id to the personId.
-        return default!;
+        return product;
     }
 }
