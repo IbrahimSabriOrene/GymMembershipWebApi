1175010d62e1190364442c63a68f408fc422df5b
IbrahimSabriOrene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-08-26 15:50:49 +0300

    Basic repo implementation (no db)

--- MemberInfo.Application/Common/Interfaces/Persistence/IProductRepository.cs
+++ MemberInfo.Application/Common/Interfaces/Persistence/IProductRepository.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MemberInfo.Domain.Products;
 
 namespace MemberInfo.Application.Common.Interfaces.Persistence;
 
 public interface IProductRepository
 {
-    
+    void Add(Product product);
 }
--- MemberInfo.Infrastructure/Persistence/ProductRepository.cs
+++ MemberInfo.Infrastructure/Persistence/ProductRepository.cs
@@ -2,10 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MemberInfo.Application.Common.Interfaces.Persistence;
+using MemberInfo.Domain.Products;
 
 namespace MemberInfo.Infrastructure.Persistence;
 
-public class ProductRepository
+public class ProductRepository : IProductRepository
 {
-    
+    private static readonly List<Product> _products = new();
+    public void Add(Product product)
+    {
+        _products.Add(product);
+    }
 }
