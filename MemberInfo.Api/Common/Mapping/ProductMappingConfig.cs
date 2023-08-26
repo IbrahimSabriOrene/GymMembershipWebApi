e77bebee254fbb98331d89ce76297b264fc4d024
IbrahimSabriOrene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-08-26 15:48:56 +0300

    Configured the mapping correctly, only issue is person Id

--- MemberInfo.Api/Common/Mapping/ProductMappingConfig.cs
+++ MemberInfo.Api/Common/Mapping/ProductMappingConfig.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Mapster;
 using MemberInfo.Application.Products.Commands.CreateProduct;
 using MemberInfo.Contracts.Products;
-using MemberInfo.Domain.Products.ValueObjects;
-
+using MemberInfo.Domain.Products;
+using PersonId = MemberInfo.Contracts.Products.PersonId;
 namespace MemberInfo.Api.Common.Mapping;
 
 public class ProductMappingConfig : IRegister
@@ -14,5 +10,14 @@ public class ProductMappingConfig : IRegister
     public void Register(TypeAdapterConfig config)
     {
         config.NewConfig<CreateProductRequest, CreateProductCommand >();
+        config.NewConfig<Product, ProductResponse>()
+        .Map(dest => dest.ProductId, src => src.Id.Value) // Map Product's Id to ProductResponse's ProductId
+        .Map(dest => dest.ProductName, src => src.ProductName) // Map Product's ProductName to ProductResponse's ProductName
+        .Map(dest => dest.Price, src => src.Prices) // Map Product's Prices to ProductResponse's Price
+        .Map(dest => dest.Months, src => src.Months) // Map Product's Months to ProductResponse's Months
+        .Map(dest => dest.CreationDate, src => src.CreationDate) // Map Product's CreationDate to ProductResponse's CreationDate
+        .Map(dest => dest.LastUpdateDate, src => src.LastUpdateDate) // Map Product's LastUpdateDate to ProductResponse's LastUpdateDate
+        .Map(dest => dest.PersonId, src => src.PersonId); // Map Product's PersonIds to ProductResponse's PersonIds
+
     }
 }
