241b7eafe07a8e7c30503abe5389f5db935bf267
İbrahim Sabri Örene <IbrahimSabriOrene@users.noreply.github.com>
2023-09-02 05:06:41 +0300

    Added hashset personIds

--- MemberInfo.Contracts/Products/CreateProductRequest.cs
+++ MemberInfo.Contracts/Products/CreateProductRequest.cs
@@ -4,8 +4,9 @@ public record CreateProductRequest
 (
     string ProductName,
     List<Prices> Price,
-    int Months
-);
+    int Months,
+    HashSet<PersonIds> PersonIds
+);//Make personId nullable
 
 //Request only has one PersonId, but the command has a list of PersonIds.
 // we are coming here but im not use why the personId is not being passed in personIdList as return
