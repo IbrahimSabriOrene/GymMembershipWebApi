e4a5d215874a992c30b2a29289b0193b470665e4
İbrahim Sabri Örene <IbrahimSabriOrene@users.noreply.github.com>
2023-09-02 05:08:21 +0300

    all gets now private set, implementation of hash

--- MemberInfo.Domain/Products/Product.cs
+++ MemberInfo.Domain/Products/Product.cs
@@ -7,13 +7,13 @@ namespace MemberInfo.Domain.Products
 {
     public sealed class Product : AggregateRoot<ProductId>
     {
-        public string ProductName { get; }
-        public List<Price> Prices { get; }
-        public int Months { get; }
-        public DateTime? CreationDate { get; }
-        public DateTime? LastUpdateDate { get; }
+        public string ProductName { get; private set; }
+        public List<Price> Prices { get; private set; }
+        public int Months { get; private set; }
+        public DateTime? CreationDate { get; private set; }
+        public DateTime? LastUpdateDate { get; private set; }
 
-        public PersonId PersonId { get; }
+        public HashSet<PersonId> PersonIds { get; private set; }
 
         public Product(
             ProductId productId,
@@ -21,15 +21,14 @@ namespace MemberInfo.Domain.Products
             List<Price> prices,
             int months,
             DateTime? creationDate,
-            DateTime lastUpdateDate,
-            PersonId personIds) : base(productId)
+            DateTime lastUpdateDate) : base(productId)
         {
             ProductName = productName;
             Prices = prices;
             Months = months;
             CreationDate = creationDate;
             LastUpdateDate = lastUpdateDate;
-            PersonId = personIds;
+            PersonIds = new HashSet<PersonId>();
         }
 
         public DateTime GetExpirationDate()
@@ -37,22 +36,38 @@ namespace MemberInfo.Domain.Products
             return CreationDate.GetValueOrDefault().AddMonths(Months);
         }
 
+        public bool AddPersonId(PersonId personId)
+        {
+            return personId is not null && PersonIds.Add(personId);
+        }
+
         public static Product Create(
             string productName,
             List<Price> prices, // price is null somehow
             int months,
-            PersonId personIds)
+            HashSet<PersonId> personIds)
         {
             var productId = ProductId.CreateUnique(); // This is the only place where we create a new ProductId
-            return new Product(
-                productId,
-                productName,
-                prices,
-                months,
-                DateTime.UtcNow,
-                DateTime.UtcNow,
-                personIds
+            var product = new Product(
+                productId: productId,
+                productName: productName,
+                prices: prices,
+                months: months,
+                creationDate: DateTime.UtcNow,
+                lastUpdateDate:DateTime.UtcNow
                 );
+
+            
+            
+            foreach (var personId in personIds)
+            {
+                product.AddPersonId(personId);
+            }
+
+            return product;
+
+
+            
         }
     }
 }
