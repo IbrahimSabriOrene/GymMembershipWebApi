f3cff044e8bf1c9372007e34d6efe45bb39d8f41
İbrahim Sabri Örene <IbrahimSabriOrene@users.noreply.github.com>
2023-09-03 23:26:07 +0300

    Made separated config for Id

--- MemberInfo.Api/Common/Mapping/ProductMappingConfig.cs
+++ MemberInfo.Api/Common/Mapping/ProductMappingConfig.cs
@@ -1,6 +1,7 @@
 using Mapster;
 using MemberInfo.Application.Products.Commands.CreateProduct;
 using MemberInfo.Contracts.Products;
+using MemberInfo.Domain.Person.ValueObjects;
 using MemberInfo.Domain.Products;
 namespace MemberInfo.Api.Common.Mapping;
 
@@ -8,11 +9,13 @@ public class ProductMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
+        config.NewConfig<PersonId, PersonIds>()
+        .Map(dest => dest.Id, src => src.Value);
         config.NewConfig<CreateProductRequest , CreateProductCommand >()
         .Map(dest => dest.ProductName, src => src.ProductName)
         .Map(dest => dest.Price, src => src.Price)
         .Map(dest => dest.Months, src => src.Months)
-        .Map(dest => dest.PersonIds, src => src.PersonIds.Select(x => x.Id).ToHashSet());
+        .Map(dest => dest.PersonIds, src => src.PersonIds.Select(x => new PersonIdsCommand(x.Id)).ToHashSet());
         config.NewConfig<Product, ProductResponse>()
         .Map(dest => dest.ProductId, src => src.Id.Value) // Map Product's Id to ProductResponse's ProductId
         .Map(dest => dest.ProductName, src => src.ProductName) // Map Product's ProductName to ProductResponse's ProductName
@@ -20,7 +23,7 @@ public class ProductMappingConfig : IRegister
         .Map(dest => dest.Months, src => src.Months) // Map Product's Months to ProductResponse's Months
         .Map(dest => dest.CreationDate, src => src.CreationDate) // Map Product's CreationDate to ProductResponse's CreationDate
         .Map(dest => dest.LastUpdateDate, src => src.LastUpdateDate) // Map Product's LastUpdateDate to ProductResponse's LastUpdateDate
-        .Map(dest => dest.PersonId, src => src.PersonIds.Select(x => x.Value).ToHashSet()); // Map Product's PersonIds to ProductResponse's PersonIds
+        .Map(dest => dest.PersonId, src => src.PersonIds.Select(x => x.Adapt<PersonIds>()).ToHashSet()); // Map Product's PersonIds to ProductResponse's PersonIds
 
     }
 }
