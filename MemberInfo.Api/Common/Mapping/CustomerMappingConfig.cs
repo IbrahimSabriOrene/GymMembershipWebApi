c25559889fd87208078ed34ff683e05d8e7c8038
İbrahim Sabri Örene <IbrahimSabriOrene@users.noreply.github.com>
2023-11-29 05:59:07 +0300

    Some fixes about customer mapping configuration

--- MemberInfo.Api/Common/Mapping/CustomerMappingConfig.cs
+++ MemberInfo.Api/Common/Mapping/CustomerMappingConfig.cs
@@ -1,29 +1,25 @@
-using Mapster;
-using Customer.Application.Products.Commands.CreateProduct;
-using Customer.Contracts.Products;
+using Customer.Contracts.Customers;
+using Customer.Domain.Person;
 using Customer.Domain.Person.ValueObjects;
-using Customer.Domain.Products;
+using Mapster;
+using MemberInfo.Application.Customers.Commands;
 namespace Customer.Api.Common.Mapping;
 
 public class CustomerMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<PersonId, PersonIds>()
-        .Map(dest => dest.Id, src => src.Value);
-        config.NewConfig<CreateProductRequest, CreateProductCommand>()
-        .Map(dest => dest.ProductName, src => src.ProductName)
-        .Map(dest => dest.Price, src => src.Price)
-        .Map(dest => dest.Months, src => src.Months)
-        .Map(dest => dest.PersonIds, src => src.PersonIds.Select(x => new PersonIdsCommand(x.Id)).ToHashSet());
-        config.NewConfig<Product, ProductResponse>()
-        .Map(dest => dest.ProductId, src => src.Id.Value) // Map Product's Id to ProductResponse's ProductId
-        .Map(dest => dest.ProductName, src => src.ProductName) // Map Product's ProductName to ProductResponse's ProductName
-        .Map(dest => dest.Price, src => src.Prices) // Map Product's Prices to ProductResponse's Price
-        .Map(dest => dest.Months, src => src.Months) // Map Product's Months to ProductResponse's Months
-        .Map(dest => dest.CreationDate, src => src.CreationDate) // Map Product's CreationDate to ProductResponse's CreationDate
-        .Map(dest => dest.LastUpdateDate, src => src.LastUpdateDate) // Map Product's LastUpdateDate to ProductResponse's LastUpdateDate
-        .Map(dest => dest.PersonId, src => src.PersonIds.Select(x => x.Adapt<PersonIds>()).ToHashSet()); // Map Product's PersonIds to ProductResponse's PersonIds
+        config.NewConfig<PersonId, CustomerId>()
+        .Map(dest => dest.Value, src => src.Value);
+        config.NewConfig<CustomerRegisterRequest, CreateCustomerCommand>()
+        .Map(dest => dest.FirstName, src => src.FirstName)
+        .Map(dest => dest.LastName, src => src.LastName)
+        .Map(dest => dest.Email, src => src.Email);
+
+
+        config.NewConfig<Person, CustomerRegisterResponse>();
 
     }
 }
+
+
