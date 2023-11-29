a3be1a0fa98fadf16bc738a51f45dc53bd92e41b
İbrahim Sabri Örene <IbrahimSabriOrene@users.noreply.github.com>
2023-11-30 02:23:12 +0300

    Addittional Mappings

--- MemberInfo.Api/Common/Mapping/CustomerMappingConfig.cs
+++ MemberInfo.Api/Common/Mapping/CustomerMappingConfig.cs
@@ -1,6 +1,7 @@
 using Customer.Contracts.Customers;
 using Customer.Domain.Person;
 using Customer.Domain.Person.ValueObjects;
+using Customer.Domain.Persons.ValueObjects;
 using Mapster;
 using MemberInfo.Application.Customers.Commands;
 namespace Customer.Api.Common.Mapping;
@@ -14,8 +15,24 @@ public class CustomerMappingConfig : IRegister
         config.NewConfig<CustomerRegisterRequest, CreateCustomerCommand>()
         .Map(dest => dest.FirstName, src => src.FirstName)
         .Map(dest => dest.LastName, src => src.LastName)
-        .Map(dest => dest.Email, src => src.Email);
-        config.NewConfig<Person, CustomerRegisterResponse>();
+        .Map(dest => dest.Email, src => src.Email)
+        .Map(dest => dest.PhoneNumber, src => src.PhoneNumber)
+        .Map(dest => dest.Value, src => src.Value);
+        config.NewConfig<Person, CustomerRegisterResponse>()
+        .Map(dest => dest.CustomerId, src => src.Id.Value)
+        .Map(dest => dest.ProductId.Value, src => src.ProductId.Value)
+        .Map(dest => dest.FirstName, src => src.FirstName)
+        .Map(dest => dest.LastName, src => src.LastName)
+        .Map(dest => dest.Email, src => src.Email)
+        .Map(dest => dest.PhoneNumber, src => src.PhoneNumber)
+        .Map(dest => dest.CreationDate, src => src.CreationDate)
+        .Map(dest => dest.LastUpdateDate, src => src.LastUpdateDate)
+        .Map(dest => dest.ExpirationDate, src => src.Expiration);
+        config.NewConfig<Expiration, DateTime>()
+        .MapWith(expiration => expiration._expirationDate);
+
+
+        // Map Person's Id to CustomerRegisterResponse's CustomerId;
 
     }
 }
