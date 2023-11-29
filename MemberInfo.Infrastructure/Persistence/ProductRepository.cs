b7e6235770bcafe4bf266c850d4c1e215402c5d1
İbrahim Sabri Örene <IbrahimSabriOrene@users.noreply.github.com>
2023-11-29 18:23:33 +0300

    Changed findbyId

--- MemberInfo.Infrastructure/Persistence/ProductRepository.cs
+++ MemberInfo.Infrastructure/Persistence/ProductRepository.cs
@@ -13,13 +13,7 @@ public class ProductRepository : IProductRepository
         _products.Add(product);
     }
 
-    public Product? FindById(ProductId productId)
-    {
-        var product = _products.FirstOrDefault(x => x.Id == productId);
-        //need if else statement
-
-        return product;
-    }
+   
 
     public bool Exists(ProductId productId)
     {
@@ -36,5 +30,16 @@ public class ProductRepository : IProductRepository
         }
         */
 
+    }
+
+    public Product? FindById(Guid Id)
+    {
+        var productId = ProductId.Insert(Id);
+        var product = _products.FirstOrDefault(x => x.Id == productId);
+        if (product is null)
+        {
+            return null;
+        }
+        else return product;
     }
 }
